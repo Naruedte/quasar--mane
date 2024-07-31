acb8507dbdf70a49658dbadb18934288ec72978f
Naruedte <maxkusg@gmail.com>
2024-07-31 10:19:30 +0700

    modified surname variable and set name and  surname text default

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -17,7 +17,7 @@
 
       <q-input
         filled
-        v-model="name"
+        v-model="surname"
         label="suranme *"
         hint="surname"
         lazy-rules
@@ -55,12 +55,14 @@ export default {
   setup () {
     const $q = useQuasar()
 
-    const name = ref(null)
+    const name = ref("นฤเดช")
+    const surname = ("คำมาวัน")
     const age = ref(null)
     const accept = ref(false)
 
     return {
       name,
+      surname,
       age,
       accept,
 
